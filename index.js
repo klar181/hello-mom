9f152904895e40f21a77d766f5102cf986bcb42c
klar181 <138244457+klar181@users.noreply.github.com>
2026-08-20 16:22:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const button = document.querySelector('button');
 const output = document.querySelector('.output');
 
 button.addEventListener('click', function() {
-    output.innerHTML = 'Мама, я тебя люблю ❤️';
+    output.innerHTML = 'Эмре, я тебя люблю ❤️';
 });
